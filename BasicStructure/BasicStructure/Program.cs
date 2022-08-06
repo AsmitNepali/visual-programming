1c8a925e9de7da7b84fd15eb61dbc4ad3c35e9a4
AsmitNepali <mail2asmitnepali@gmail.com>
2022-08-06 23:45:59 +0545

    ADD[Variables]: Add New Project.

---
+++ BasicStructure/BasicStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicStructure
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            //my first prgram in C#
+            Console.WriteLine("Hello World");
+            Console.Write("Hello Asmit ");
+            Console.Write("Nepali");
+
+        }
+    }
+        
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Varibles;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2;
+
+        num1 = 5;
+        num2 = 6;
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"Total: {sum}");
+    }
+}
