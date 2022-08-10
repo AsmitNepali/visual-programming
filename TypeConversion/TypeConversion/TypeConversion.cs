1a2545cd4828cf8f922e0fa8c0538a90d6916bf4
AsmitNepali <mail2asmitnepali@gmail.com>
2022-08-10 20:00:51 +0545

    IMP: Data Conversion.

---
+++ TypeConversion/TypeConversion/TypeConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class TypeConversion
+    {
+        static void Main(String[] args)
+        {
+            //char->int->long->float->double #Implicit
+            //double->float->long->int->char #Explicit
+
+
+            int myInt = 99;
+            double myDouble = myInt; //Automatically convert into double
+
+
+            double yourDouble = 99.99;
+            int yourInt = (int) yourDouble;
+
+            Console.WriteLine(myDouble);
+            Console.WriteLine(yourInt);
+
+            // Conversion method
+
+            //Console.WriteLine(Convert.ToString(yourInt));
+            //Console.WriteLine(myDouble.ToString());
+
+        }
+    }
+}
