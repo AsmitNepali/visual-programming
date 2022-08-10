692dafa4a85a0c31b4948e9485483f465175370b
AsmitNepali <mail2asmitnepali@gmail.com>
2022-08-10 20:09:49 +0545

    IMP: String Type.

---
+++ StringType/StringType/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringType
+{
+    class Progarm
+    {
+        static void Main(String[] args)
+        {
+            String str = "Hello\rWorld!";
+
+            String myString = @"Hello\rWorld!";
+
+            int num1 = 5;
+            int num2 = 4;
+
+            Console.WriteLine($"Sum: {num1 + num2 }");
+
+            Console.WriteLine(str);
+            Console.WriteLine(myString);
+        }
+    }
+}
